6e27a539c6f18d35c0bc7fc5713484cc6150dc50
IT24100203 <it24100203@my.sliit.lk>
2024-09-21 13:46:55 +0530

    Add Lab7Q2A/Q2B/Q2C.java to Q2A/Q2B/Q2C

---
+++ Q2/IT24100203Lab7Q2A.java
@@ -0,0 +1,11 @@
+public class IT24100203Lab7Q2A {
+   public static void main (String[]args) {
+	   for (int i=1; i<=5; i++) {
+		   for (int j=1; j<=5; j++) {
+			   System.out.print("$ ");
+		   }
+		// Move to the next line after each row
+		   System.out.println();
+	   }
+   }
+}
---
+++ Q2/IT24100203Lab7Q2C.java
@@ -0,0 +1,11 @@
+public class IT24100203Lab7Q2C {
+   public static void main (String[]args) {
+	   // Outer loop to handle the number of lines (starting from 5)
+	   for (int i=5; i>=1; i--) {
+		   for (int j=1; j<=i; j++) {
+			   System.out.print(i); //to print the current line
+		   }
+		   System.out.println("");
+	   }
+   }
+}
